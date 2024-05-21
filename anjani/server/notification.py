9f73a1f9fae91782fe7b2af07f37eee41704963d
oneforalone <oneforalone@proton.me>
2024-05-21 07:24:32 +0000

    fix: change to telegram user inline mention in lottery message notify

--- anjani/server/notification.py
+++ anjani/server/notification.py
@@ -1,10 +1,6 @@
 from datetime import datetime, timezone
 
 
-class ArgumentTypeError(Exception):
-    pass
-
-
 def build_lottery_create_msg(template: str, **args) -> str:
     community_name = args.get("communityName", "")
     prize = args.get("prize")
@@ -16,20 +12,22 @@ def build_lottery_create_msg(template: str, **args) -> str:
 
 
 def build_lottery_join_msg(template: str, **args) -> str:
-    nick_names = args.get("nickNames")
+    tg_users = args.get("tgUsers")
     # type check
-    if not isinstance(nick_names, list) and not isinstance(nick_names, str):
-        raise ArgumentTypeError("nick_names should be a list or string")
+    assert isinstance(tg_users, list), "tgUsers should be a list"
 
-    if isinstance(nick_names, list):
-        nick_names = ", @".join(nick_names)
+    joined_users = ""
+    for tg_user in tg_users:
+        nick = tg_user.get("nick", "")
+        tg_id = tg_user.get("tgId", "")
+        joined_users += f"[{nick}](tg://user?id={tg_id}) has entered the luckydraw.\n"
 
     end_time_ms = args.get("endTime", 0)
     end_time = format_msg_timestamp(end_time_ms)
 
     prize = args.get("prize")
 
-    return template.format(nick_names=nick_names, end_time=end_time, prize=prize)
+    return template.format(joined_users=joined_users, end_time=end_time, prize=prize)
 
 
 def build_lottery_end_msg(template: str, **args) -> str:
