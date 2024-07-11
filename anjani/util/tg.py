7104c2f53d2a4e22efbfa874f45742fd1c1c912d
oneforalone <oneforalone@proton.me>
2024-07-11 09:45:06 +0000

    replace Beecon to Banknote Plane

--- anjani/util/tg.py
+++ anjani/util/tg.py
@@ -354,7 +354,7 @@ def get_text(
             if lang == "en":
                 return (
                     f"**NO LANGUAGE STRING FOR '{text_name}' in '{lang}'**\n"
-                    "Beecon"
+                    "Banknote Plane"
                 )
 
             bot.log.warning("NO LANGUAGE STRING FOR '%s' in '%s'", text_name, lang)
