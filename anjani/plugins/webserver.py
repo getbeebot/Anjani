6a64a2ac462522b139254a9d3988f09947341996
oneforalone <oneforalone@proton.me>
2024-07-17 10:00:57 +0000

    migrate web server to plugin

--- anjani/plugins/webserver.py
+++ anjani/plugins/webserver.py
@@ -1,4 +1,7 @@
-from typing import ClassVar
+import os
+import aiofiles.os as aio_os
+import base64
+from typing import ClassVar, Optional
 
 from datetime import datetime, timezone
 
@@ -7,7 +10,24 @@ from aiohttp import web
 from aiohttp import WSMsgType
 from aiohttp.web import BaseRequest, Response
 
-from anjani import plugin, util
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from anjani import plugin
+from anjani.util.db import MysqlPoolClient
+from anjani.util.project_config import BotNotificationConfig
+from anjani.server.notification import (
+    build_congrats_msg,
+    build_congrats_records,
+    build_invitation_notify,
+    build_invitation_records,
+    build_lottery_create_msg,
+    build_lottery_end_msg,
+    build_lottery_join_msg,
+)
+from anjani.language import get_template
+
+import boto3
 
 class WebServer(plugin.Plugin):
     name: ClassVar[str] = "WebServer"
@@ -16,7 +36,18 @@ class WebServer(plugin.Plugin):
     ws_clients: set
     site: web.TCPSite
 
+    s3: any
+
+    mysql: MysqlPoolClient
+
     async def on_load(self) -> None:
+        # init for aws s3
+        self.s3 = boto3.client("s3", region_name="ap-southeast-1", aws_access_key_id=self.bot.config.AWS_AK, aws_secret_access_key=self.bot.config.AWS_SK)
+        # init for mysql
+        self.mysql = MysqlPoolClient.init_from_env()
+        # init for ws clients
+        self.ws_clients = set()
+        # init web server
         app = web.Application()
 
         is_member_router = web.post("/is_member", self.is_member_handler)
@@ -25,7 +56,6 @@ class WebServer(plugin.Plugin):
         get_invite_link_router = web.post("/get_invite_link", self.get_invite_link_handler)
         privilege_check_router = web.post("/check_bot_privilege", self.privilege_check_handler)
 
-        # TODO: add websockect
         ws_router = web.get("/ws", self.project_creation_notify)
 
         routers = [
@@ -49,8 +79,7 @@ class WebServer(plugin.Plugin):
         host = self.bot.config.WEB_HOST
         port = self.bot.config.WEB_PORT
         self.site = web.TCPSite(runner, host, port)
-        # init for ws clients
-        self.ws_clients = set()
+
 
         await self.site.start()
 
@@ -90,10 +119,6 @@ class WebServer(plugin.Plugin):
             user = await self.bot.client.get_users(user_id)
             username = user.username if user.username else None
             avatar_link = await self.get_avatar(user_id)
-
-            if not avatar_link:
-                avatar_link = ""
-
             firstname = user.first_name or ""
             lastname = user.last_name
             fullname = firstname + " " + lastname if lastname else firstname
@@ -112,8 +137,185 @@ class WebServer(plugin.Plugin):
         web.json_response(ret_data, status=200)
 
     async def send_msg_handler(self, request: BaseRequest) -> Response:
-        # TODO:
-        pass
+        ret_data = { "ok": False }
+        try:
+            payloads = await request.json()
+            self.log.info("/sendmsg request payloads: %s", payloads)
+
+            chat_type = payloads.get("type")
+
+            if not chat_type:
+                return web.json_response({"ok": False, "error": "No type argument, please checkout the request arguments."}, status=200)
+
+            assert isinstance(chat_type, int), "type argument should be int, but got an non-int, please check it out"
+
+            data = payloads.get("data")
+
+            chat_id = payloads.get("chatId")
+
+            if not chat_id:
+                chat_id = data.get("owner")
+
+            if not chat_id:
+                return web.json_response({"ok": False, "error": "No chat_id or owner, please checkout the request arguments."}, status=200)
+
+            assert isinstance(chat_id, str) or isinstance(chat_id, int), "chatId/data.owner not found or not correct type"
+
+            chat_id = int(chat_id)
+
+            uri = data.get("uri") or os.getenv("TWA_LINK")
+
+            button = InlineKeyboardMarkup([[InlineKeyboardButton("🕹 Enter", url=uri)]])
+
+            project_id = await self.bot.mysql.get_chat_project_id(chat_id)
+            project_config = await BotNotificationConfig.get_project_config(self.mysql, project_id)
+            if not project_config:
+                project_config = BotNotificationConfig(project_id)
+
+            chat = await self.bot.client.get_chat(chat_id)
+            if chat.type == ChatType.CHANNEL:
+                self.log.warn("Chat type is channel, not sending notification. Channel: %s, content: %s", chat.title, payloads)
+                return web.json_response({"ok": False, "error": "I'm not push notification to channel"}, status=200)
+
+            content: str = ""
+            notify_type = data.get("notifyType")
+            # engage_img_link = await get_template("engage-img")
+            engage_img_link = os.getenv("ENGAGE_IMG", "https://beeconavatar.s3.ap-southeast-1.amazonaws.com/engage.png")
+
+            lucky_draw_btn = InlineKeyboardButton(text="View the luckydraw", url=uri)
+
+            withdraw_btn = InlineKeyboardButton(text="Withdraw", url=f"t.me/beecon_wallet_bot?start=true")
+
+            lottery_type = data.get("lotteryType")
+            if notify_type == 1 and project_config.newdraw:    # create lottery task
+                template = await get_template(f"lottery-create-{lottery_type}")
+                content = build_lottery_create_msg(template, **data)
+                self.log.info(f"sending message {content}")
+
+                await self.bot.client.send_photo(
+                    chat_id,
+                    engage_img_link,
+                    caption=content,
+                    reply_markup=button,
+                )
+
+                ret_data = ret_data.update({"ok": True})
+            elif notify_type == 2 and project_config.userjoin:  # user entered the draw
+                template = await get_template(f"lottery-join-{lottery_type}")
+                content = build_lottery_join_msg(template, **data)
+                self.log.info(f"sending message {content}")
+
+                await self.bot.client.send_photo(
+                    chat_id,
+                    engage_img_link,
+                    caption=content,
+                    reply_markup=button,
+                )
+
+                ret_data = ret_data.update({"ok": True})
+            elif notify_type == 3 and project_config.draw:  # lottory draw winner announce
+                template = await get_template("lottery-end")
+                content = build_lottery_end_msg(template, **data)
+
+                # luckdraw_img_link = await get_template("luckydraw-img")
+                luckdraw_img_link = os.getenv("DRAW_IMG", "https://beeconavatar.s3.ap-southeast-1.amazonaws.com/luckydraw.png")
+
+                self.log.info(f"sending message {content}")
+
+                await self.bot.client.send_photo(
+                    chat_id,
+                    luckdraw_img_link,
+                    caption=content,
+                    reply_markup=button
+                )
+                ret_data.update({"ok": True})
+            elif notify_type == 4:  # sending file to community admin
+                filename = data.get("lotteryFileName")
+                chat_id = data.get("owner")
+                base_link = os.getenv("WEBSITE")
+                download_link = f"{base_link}/downloads/{filename}"
+                content = f"Please download the winners data via {download_link}"
+
+                await self.bot.client.send_message(chat_id, content)
+
+                self.log.info(f"send {download_link} to {chat_id}")
+
+                ret_data.update({"ok": True})
+            elif notify_type == 5 and project_config.newtask:
+                content = await get_template("task-creation")
+                await self.bot.client.send_photo(
+                    chat_id,
+                    engage_img_link,
+                    caption=content,
+                    reply_markup=button,
+                )
+                ret_data.update({"ok": True})
+            elif notify_type == 6: # private congrats
+                template = await get_template("congrats-draw")
+                content = build_congrats_msg(template, **data)
+
+                keyboard = InlineKeyboardMarkup([
+                    [lucky_draw_btn],
+                    [withdraw_btn]
+                ])
+                await self.bot.client.send_photo(
+                    chat_id=chat_id,
+                    photo=os.getenv("UNION_DRAW_IMG"),
+                    caption=content,
+                    reply_markup=keyboard,
+                )
+                ret_data.update({"ok": True})
+            elif notify_type == 7: # congrats records
+                template = await get_template("congrats-records")
+                content = build_congrats_records(template, **data)
+
+                keyboard = InlineKeyboardMarkup([
+                    [lucky_draw_btn],
+                    [withdraw_btn],
+                ])
+                await self.bot.client.send_message(
+                    chat_id=chat_id,
+                    content=content,
+                    reply_markup=keyboard,
+                )
+                ret_data.update({"ok": True})
+            elif notify_type == 8: # invitation records
+                template = await get_template("invitation-records")
+                content = build_invitation_records(template, **data)
+
+                keyboard = InlineKeyboardMarkup([
+                    [lucky_draw_btn],
+                ])
+                await self.bot.client.send_message(
+                    chat_id=chat_id,
+                    content=content,
+                    reply_markup=keyboard,
+                )
+                ret_data.update({"ok": True})
+            elif notify_type == 9: # invitation success notify
+                template = await get_template("invitation-notify")
+                content = build_invitation_notify(template, **data)
+
+                keyboard = InlineKeyboardMarkup([
+                    [lucky_draw_btn]
+                ])
+                await self.bot.client.send_message(
+                    chat_id=chat_id,
+                    content=content,
+                    reply_markup=keyboard
+                )
+                ret_data.update({"ok": True})
+            else:
+                self.log.warn("Not push notification for request: %s", payloads)
+                ret_data.update({"ok": False, "error": "reject by setting"})
+
+        except Exception as e:
+            self.log.error(f"Sending occurs error: {str(e)}")
+            ret_data.update({
+                "ok": False,
+                "error": str(e),
+            })
+        return web.json_response(ret_data, status=200)
 
     async def get_invite_link_handler(self, request: BaseRequest) -> Response:
         ret_data = { "ok": False }
@@ -153,11 +355,36 @@ class WebServer(plugin.Plugin):
             payloads = await request.json()
             self.log.info("/check_bot_privilege request payloads: %s", payloads)
 
+            chat_id = payloads.get("chatId")
+            member = await self.bot.client.get_chat_member(int(chat_id), self.bot.uid)
 
+            bot_privileges = member.privileges
 
+            self.log.debug("Bot %s in chat %s: %s ", self.bot.uid, chat_id, member)
+
+            privileges = [
+                "can_manage_chat",
+                "can_delete_messages",
+                "can_restrict_members",
+                # "can_change_info",
+                "can_invite_users",
+                # "can_pin_messages",
+            ]
+
+            for privilege in privileges:
+                if not bot_privileges.__getattribute__(privilege):
+                    ret_data.update({
+                        "ok": False,
+                        "error": f"bot does not have privilege: {privilege}",
+                    })
+                    return web.json_response(ret_data, status=200)
+
+            ret_data.update({"ok": True})
         except Exception as e:
-            self.log.error("")
-        pass
+            self.log.error("Bot privileges check error: %s", str(e))
+            ret_data.update({"ok": False, "error": str(e)})
+
+        return web.json_response(ret_data, status=200)
 
     async def project_creation_notify(self, request: BaseRequest) -> Response:
         ws = web.WebSocketResponse()
@@ -184,7 +411,57 @@ class WebServer(plugin.Plugin):
             self.log.error(f"Websocket connection closed, {e}")
 
     async def send_alert_handler(self, request: BaseRequest) -> Response:
-        pass
+        ret_data = {"ok": False}
+        try:
+            url = os.getenv("ALERT_API")
+            user = os.getenv("ALERT_USER")
+            password = os.getenv("ALERT_PASS")
 
-    async def get_avatar(self, user_id: int):
-        pass
+            payloads = await request.json()
+
+            auth_token = base64.b64encode(f"{user}:{password}".encode("utf-8")).decode("utf-8")
+            auth = f"Basic {auth_token}"
+            headers = {
+                "Authorization": auth,
+                "Content-Type": "application/json",
+            }
+            async with self.bot.http.post(url, json=payloads, headers=headers) as resp:
+                self.log.info(f"Alert response: %s", resp)
+                if resp.status == 200:
+                    ret_data.update({"ok": True})
+                else:
+                    ret_data.update({"ok": False, "error": await resp.text()})
+        except Exception as e:
+            self.log.error(f"push alert error: {e}")
+            ret_data.update({
+                "ok": False,
+                "error": str(e)
+            })
+        web.json_response(ret_data, status=200)
+
+    async def get_avatar(self, chat_id: int) -> Optional[str]:
+        avatar_link = None
+        try:
+            photos = self.bot.client.get_chat_photos(chat_id)
+            photo = await photos.__anext__()
+            if photo.file_id:
+                avatar_name = f"B{chat_id}.jpg"
+                avatar = f"/app/downloads/{avatar_name}"
+
+                await self.client.download_media(photo.file_id, file_name=avatar)
+
+                s3_bucket = self.bot.config.AWS_S3_BUCKET
+                self.s3.upload_file(
+                    avatar,
+                    s3_bucket,
+                    avatar_name,
+                    ExtraArgs={"ContentType": "image/jpeg"}
+                )
+
+                await aio_os.remove(avatar)
+
+                avatar_link = f"https://{s3_bucket}.s3.ap-southeast-1.amazonaws.com/{avatar_name}"
+        except Exception as e:
+            self.log.warn("Get chat %s avatar link error %s", chat_id, e)
+
+        return avatar_link
