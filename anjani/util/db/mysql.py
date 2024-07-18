3b42549a66d94d8a7f2db031ce35d38fa61050bf
oneforalone <oneforalone@proton.me>
2024-07-18 05:29:27 +0000

    filter out deleted tasks for cron_job

--- anjani/util/db/mysql.py
+++ anjani/util/db/mysql.py
@@ -125,7 +125,7 @@ class MysqlPoolClient:
             return None
 
     async def get_project_ids(self, bot_id: int):
-        sql = "SELECT DISTINCT bp.id, target_id FROM bot_project AS bp JOIN beebot.tz_user_tg_group AS tutg ON bp.target_id = tutg.chat_id WHERE bp.target_id IS NOT NULL AND tutg.bot_id=%s AND tutg.chat_type=0"
+        sql = "SELECT DISTINCT bp.id, bp.target_id FROM bot_project AS bp JOIN beebot.tz_user_tg_group AS tutg ON bp.target_id = tutg.chat_id WHERE bp.target_id IS NOT NULL AND bp.deleted = 0 AND tutg.chat_type = 0 AND tutg.bot_id=%s"
         res = await self.query(sql, (bot_id, ))
         return res
 
