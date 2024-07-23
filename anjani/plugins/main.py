0867f003c23795ecb938299c3bccaa92373bf4f5
oneforalone <oneforalone@proton.me>
2024-07-23 05:13:29 +0000

    change X to emoji 𝕏

--- anjani/plugins/main.py
+++ anjani/plugins/main.py
@@ -631,7 +631,7 @@ class Main(plugin.Plugin):
                 [
                     InlineKeyboardButton(text = await self.text(chat.id, "faq-button"), url=faq_link),
                     InlineKeyboardButton(text=await self.text(chat.id, "channel-button"), url=channel_link),
-                    InlineKeyboardButton(text="X", url=f"https://x.com/{x_username}")
+                    InlineKeyboardButton(text="𝕏", url=f"https://x.com/{x_username}")
                 ],
                 [
                     InlineKeyboardButton(text=await self.text(None, "forkme-button"), callback_data="help_forkme")
