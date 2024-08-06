e1ce607c51ccfe3b8c11eaa8224b95e2b29545fb
oneforalone <oneforalone@proton.me>
2024-08-06 12:12:50 +0000

    fix: session file not working

--- anjani/core/anjani_bot.py
+++ anjani/core/anjani_bot.py
@@ -72,7 +72,7 @@ class Anjani(TelegramBot, DatabaseProvider, PluginExtender, CommandDispatcher, E
 
         try:
             # restore session for anjani to avoid peer id loss
-            await misc.session_restore()
+            # await misc.session_restore()
 
             anjani = cls(config)
             await anjani.run()
