a8c8736f0e2e30b623d72e653b5c0a227ca0f6b5
oneforalone <oneforalone@proton.me>
2024-08-15 14:26:35 +0800

    fix: try use clear() method to reduce alert records

--- anjani/plugins/webserver.py
+++ anjani/plugins/webserver.py
@@ -202,11 +202,11 @@ class WebServer(plugin.Plugin):
     async def auto_solve_alert(self, name, trace_id, desc) -> None:
         # auto resolve alert after 30 seconds
         await asyncio.sleep(30)
-        self.event_counter.labels(name=name, trace_id=trace_id, desc=desc).reset()
+        self.event_counter.labels(name=name, trace_id=trace_id, desc=desc).clear()
 
     async def auto_remove_gague(self, method, path, trace_id) -> None:
         await asyncio.sleep(300)
-        self.api_rtt_gauge.labels(method=method, path=path, trace_id=trace_id).remove()
+        self.api_rtt_gauge.labels(method=method, path=path, trace_id=trace_id).clear()
 
     async def is_member_handler(self, request: BaseRequest) -> Response:
         ret_data = {"ok": False}
