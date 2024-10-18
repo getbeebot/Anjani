a776df89940e45b580bdd403995dfe2467e2631f
oneforalone <oneforalone@proton.me>
2024-10-18 12:56:11 +0800

    remove test constraint

--- anjani/custom_plugins/feedback.py
+++ anjani/custom_plugins/feedback.py
@@ -121,9 +121,6 @@ We will seriously consider all your feedback.
         for u in users:
             try:
                 user_chat_id = int(u[0])
-                # for testing
-                if user_chat_id != 6812515288:
-                    continue
                 await self.bot.client.send_photo(
                     chat_id=user_chat_id, photo=pic, caption=msg, reply_markup=buttons
                 )
